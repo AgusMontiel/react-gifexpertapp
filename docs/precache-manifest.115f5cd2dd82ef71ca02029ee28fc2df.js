7b1b06d4ff2765c649d50b2fe1e649b63744592e
Agustina <agustina.gisela1@gmail.com>
2020-11-04 05:29:18 -0300

    Docs agregado

---
+++ docs/precache-manifest.115f5cd2dd82ef71ca02029ee28fc2df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a159d2036fdb0b8a1562d50b02f5d454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea1d7f9c25a78cb2d0f6",
+    "url": "/static/css/main.38eb90b3.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1d7f9c25a78cb2d0f6",
+    "url": "/static/js/main.b0e386ec.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
